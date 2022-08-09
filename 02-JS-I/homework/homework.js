5a6e81c4fbb39f878a6efde8c4db114858ce296d
rodrigosdw <rodrigosdw@gmail.com>
2022-08-09 19:20:59 -0300

    resuelto hasta combinarNombres

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -174,12 +174,15 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var combinado = nombre + " " + apellido;
+  return combinado;
 }
 
 function obtenerSaludo(nombre) {
